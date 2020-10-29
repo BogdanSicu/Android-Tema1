680f4fbda5057f0e10eea7b6b3fb3659d594e8a0
Sicu Bogdan <b0gdan.munt3@gmail.com>
2020-10-29 21:59:39 +0200

    yes butoane

--- tema1/app/src/main/java/com/example/quiz/Intrebare.java
+++ tema1/app/src/main/java/com/example/quiz/Intrebare.java
@@ -2,13 +2,35 @@ package com.example.quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Intrebare extends AppCompatActivity {
+
+    private Button quitAndSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intrebare);
+
+        quitAndSave =findViewById(R.id.quit_and_save_btn);
+        quitAndSave.setVisibility(View.VISIBLE);
+        quitAndSave.setBackgroundColor(Color.TRANSPARENT);
+
+        quitAndSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                quit_and_save();
+            }
+        });
+    }
+
+    public void quit_and_save(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- tema1/app/src/main/java/com/example/quiz/MainActivity.java
+++ tema1/app/src/main/java/com/example/quiz/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonQuiz;
+    private Button buttonResults;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonQuiz = findViewById(R.id.main_button);
+        buttonQuiz.setVisibility(View.VISIBLE);
+        buttonQuiz.setBackgroundColor(Color.TRANSPARENT);
+        buttonQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openQuiz();
+            }
+        });
+
+        buttonResults= findViewById(R.id.results_button);
+        buttonResults.setBackgroundColor(View.VISIBLE);
+        buttonResults.setBackgroundColor(Color.TRANSPARENT);
+        buttonResults.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openResults();
+            }
+        });
+    }
+
+    public void openQuiz(){
+        Intent intent = new Intent(this, Intrebare.class);
+        startActivity(intent);
+    }
+
+    public void openResults(){
+        Intent intent = new Intent(this, Results_activity.class);
+        startActivity(intent);
     }
 }
